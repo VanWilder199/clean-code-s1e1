303fb3facd75f99be2d0b1b2a0e6c5e8e9bc2291
VanWilder199 <kublitskiyvladimir@gmail.com>
2021-04-03 17:37:50 +0300

    refactor: add indents  to rule 3.5 in html-css

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ var createNewTaskElement=function(taskString){
     checkBox.type="checkbox";
     checkBox.className = "checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="task-area";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="edit";
